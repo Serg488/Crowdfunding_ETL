76a78a12005e2a02f8b7385c5d824e88b3e3a4f7
pamelafm <34518912+pamelafm@users.noreply.github.com>
2024-09-24 10:14:07 -0500

    assignment commit

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,40 +1,36 @@
-CREATE TABLE "Contacts" (
-    "contact_id" INT NOT NULL,
-    "first_name" VARCHAR(50) NOT NULL,
-    "last_name" VARCHAR(50) NOT NULL,
-    "email" VARCHAR(50) NOT NULL,
-    CONSTRAINT "pk_Contacts" PRIMARY KEY ("contact_id")
-);
+CREATE TABLE category(
+category_id VARCHAR NOT NULL,
+category VARCHAR NOT NULL,
+PRIMARY KEY(category_id));
 
-CREATE TABLE "category" (
-    "category_id" VARCHAR(50) NOT NULL,
-    "category" VARCHAR(50) NOT NULL,
-    CONSTRAINT "pk_category" PRIMARY KEY ("category_id")
-);
+CREATE TABLE subcategory(
+subcategory_id VARCHAR NOT NULL,
+Subcategory VARCHAR NOT NULL,
+PRIMARY KEY(subcategory_id));
 
-CREATE TABLE "subcategory" (
-    "subcategory_id" VARCHAR(50) NOT NULL,
-    "sub-category" VARCHAR(50) NOT NULL,
-    CONSTRAINT "pk_subcategory" PRIMARY KEY ("subcategory_id")
-);
+CREATE TABLE contacts(
+contact_id INT NOT NULL,
+first_name VARCHAR NOT NULL,
+last_name VARCHAR NOT NULL,
+email VARCHAR NOT NULL,
+PRIMARY KEY (contact_id));
 
-CREATE TABLE "Campaign" (
-    "cf_id" INT NOT NULL,
-    "contact_id" INT NOT NULL,
-    "company" VARCHAR(50) NOT NULL,
-    "description" VARCHAR(255) NOT NULL,
-    "goal" FLOAT NOT NULL,
-    "pledged" FLOAT NOT NULL,
-    "outcome" VARCHAR(50) NOT NULL,
-    "backers_count" INT NOT NULL,
-    "country" VARCHAR(50) NOT NULL,
-    "currency" VARCHAR(50) NOT NULL,
-    "launched_date" DATE NOT NULL,
-    "end_date" DATE NOT NULL,
-    "category_id" VARCHAR(50) NOT NULL,
-    "subcategory_id" VARCHAR(50) NOT NULL,
-    CONSTRAINT "pk_Campaign" PRIMARY KEY ("cf_id"),
-    CONSTRAINT "fk_Campaign_Contacts" FOREIGN KEY ("contact_id") REFERENCES "Contacts" ("contact_id"),
-    CONSTRAINT "fk_Campaign_category" FOREIGN KEY ("category_id") REFERENCES "category" ("category_id"),
-    CONSTRAINT "fk_Campaign_subcategory" FOREIGN KEY ("subcategory_id") REFERENCES "subcategory" ("subcategory_id")
-);
+CREATE TABLE campaign(
+cf_id INT NOT NULL,
+contact_id INT NOT NULL,
+company_name VARCHAR NOT NULL,
+description VARCHAR NOT NULL,
+goal FLOAT NOT NULL,
+pledged FLOAT NOT NULL,
+outcome VARCHAR NOT NULL,
+backers_count INT NOT NULL ,
+country VARCHAR NOT NULL,
+currency VARCHAR NOT NULL,
+launched_date DATE NOT NULL,
+end_date DATE NOT NULL,
+category_id VARCHAR NOT NULL,
+subcategory_id VARCHAR NOT NULL,
+PRIMARY KEY(cf_id),
+FOREIGN KEY(contact_id) REFERENCES contacts(contact_id),
+FOREIGN KEY(category_id) REFERENCES category(category_id),
+FOREIGN KEY(subcategory_id) REFERENCES subcategory(subcategory_id));
